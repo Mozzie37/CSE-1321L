5231012d0281f217ac7446bcf044de80fe69cf05
Mozzie Osaba <noahosaba@gmail.com>
2024-03-25 22:41:26 -0400

    lab 12 partially done

--- Lab12/Lab12A.java
+++ Lab12/Lab12A.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
+
+
+//todo FINISH THIS 
 public class Lab12A {
+    public static void main(String[] args) {
+        int legs;
+        boolean rolling;
+        String material;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("You are about to create a chair.");
+        System.out.print("How many legs does your chair have: ");
+        legs = in.nextInt();
+        in.next();
+        System.out.print("Is your chair rolling (true/false) : ");
+        rolling = in.nextBoolean();
+        in.next();
+        System.out.println("What is your chair made of: ");
+        material = in.nextLine();
+        in.next();
+        
+
+
+
+
+
+
+
+    }
+}
+class Chair{
+    //attributes
+    public int numOfLegs;
+    public boolean rolling;
+    public String material;
+
+
+    //constructor
+    Chair(){
+        numOfLegs = 0;
+        rolling = false;
+        material = null;
+    }
 }
