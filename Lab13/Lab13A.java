e89f1798496e9f39f0e779e58393f670d75d803c
Mozzie Osaba <roberto.osaba.03@gmail.com>
2024-04-08 20:21:49 -0400

    all labs finished for semester :)

--- Lab13/Lab13A.java
+++ Lab13/Lab13A.java
@@ -1,4 +1,22 @@
 public class Lab13A {
+    public static void main(String[] args){
+        BuildingBlueprint buildingOne = new BuildingBlueprint();
+        BuildingBlueprint buildingTwo = new BuildingBlueprint(30, 30, 0.75f);
+
+        System.out.println("Year 2020:");
+        System.out.println("Building 1 has " + buildingOne.getStories() + " floors, " + buildingOne.getApartments() + " apartments and is " + (100 * buildingOne.getOccupancy()) + "% occupied. Full? " + buildingOne.getFull());
+        System.out.println("Building 2 has " + buildingTwo.getStories() + " floors, " + buildingTwo.getApartments() + " apartments and is " + (100 * buildingTwo.getOccupancy()) + "% occupied. Full? " + buildingTwo.getFull());
+
+        System.out.println("\nMany years pass.\n");
+
+        buildingOne.setOccupancy(0);
+        buildingTwo.setOccupancy(1);
+        System.out.println("Building 1 has " + buildingOne.getStories() + " floors, " + buildingOne.getApartments() + " apartments and is " + (100 * buildingOne.getOccupancy()) + "% occupied. Full? " + buildingOne.getFull());
+        System.out.println("Building 2 has " + buildingTwo.getStories() + " floors, " + buildingTwo.getApartments() + " apartments and is " + (100 * buildingTwo.getOccupancy()) + "% occupied. Full? " + buildingTwo.getFull());
+
+        System.out.println("Looks like people prefer taller buildings.");
+
+    }
 }
 
 
@@ -24,21 +42,22 @@ class BuildingBlueprint{
 
     //setters
     void setOccupancy(float percentOccupancy) {
-
+        occupancy = percentOccupancy;
+        full = (occupancy == 1);
     }
 
     //getters
 
-    void getOccupancy(){
-
+    float getOccupancy(){
+        return occupancy;
     }
-    void getStories(){
-
+    int getStories(){
+        return stories;
     }
-    void getApartments(){
-
+    int getApartments(){
+        return apartments;
     }
-    void getFull(){
-
+    boolean getFull(){
+        return full;
     }
 }
